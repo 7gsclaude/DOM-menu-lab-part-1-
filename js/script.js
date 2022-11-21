12161a20a2c12ab2b1069d381bde4c1b55f05e14
Claude Allen <claudeallen@Claudes-MacBook-Pro.local>
2022-11-20 18:19:46 -0600

    solved the homework

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+3.0
+// Menu data structure
+var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
+
+
 // Task 1.0
 //Select and cache the <main>element in a variable named mainEl
 
@@ -11,10 +21,45 @@ console.log(mainEl);
 // Hint: Assign a string that uses the CSS var()function like this:
 // 'var(--main-bg)'
 
-//i dont understand how i couldnt get it to work by making the color a variablee and using it like the intro says. 
-mainEl.style.backgroundColor = '#4a4e4d';
+//idk how i it didnt work by making the color a variablee and using it like the intro says. 
+mainEl.style.backgroundColor = 'var(--main-bg)';
 
 // Task 1.2
 // Set the content of mainEl to <h1>SEI Rocks!</h1>.
 
 mainEl.innerHTML = '<h1>SEI ROCKS</h1>'
+
+// Task 1.3
+// Add a class of flex-ctrto mainEl.
+
+mainEl.classList.add("flex-ctr");
+
+// Task 2.0
+// Select and cache (just name the topname variabel? ) the <nav id="top-menu"> element in a variable named topMenuEl.
+
+let topMenuEl = document.getElementById('top-menu');
+
+// Task 2.1
+// Set the height topMenuElelement to be 100%.
+
+topMenuEl.style.height = "100%";
+
+// Task 2.2
+// Set the background color of topMenuEl to the value stored in the --top-menu-bgCSS custom property.
+
+
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)'; // similar error here, not sure why im not getting thhe color from the property
+
+// Task 2.3
+// Add a class of flex-aroundto topMenuEl
+
+topMenuEl.classList.add("flex-around");
+
+for(let i= 0; i <= 3; i++){
+    const links = menuLinks[i]; //creating a variable 
+    const anchor = document.createElement("a"); //createes anchor tag
+    anchor.innerHTML = links.text;
+    anchor.href = links.href;
+    topMenuEl.appendChild(anchor);
+  }
+
